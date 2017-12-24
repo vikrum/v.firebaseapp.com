30bf62d05333db550381e6d4074c312883a7111c
Vikrum Nijjar <vsnijjar@gmail.com>
2017-12-23 20:57:02 -0800

    Added QR.

--- functions/index.js
+++ functions/index.js
@@ -4,3 +4,19 @@ exports.req = functions.https.onRequest((request, response) => {
   response.setHeader('content-type', 'application/json');
   response.send(JSON.stringify(request.headers, null, 2));
 });
+
+exports.rand = functions.https.onRequest((request, response) => {
+  var rand = { };
+  rand.uuid = uuid();
+  rand.float = Math.random(); 
+  rand.time = Date.now();
+  response.setHeader('content-type', 'application/json');
+  response.send(JSON.stringify(rand, null, 2));
+});
+
+function uuid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
+}
